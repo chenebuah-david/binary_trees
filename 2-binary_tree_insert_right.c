dda67524fb5a8c8e9d3ab4e0431bddeea6b33a40
chenebuah-david <davidchenebuah@gmail.com>
2023-08-30 15:09:44 -0700

    David_and_Nina

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Insert node as a right-child
+ *                             in the binary tree.
+ * @parent: A pointer to the node to insert the right-child in.
+ * @value: The value to keep the new node.
+ *
+ * Return: If parent is NULL or an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ *
+ * Description: If parent already has a right-child, new node will
+ *              take its place and the old right-child will be set as
+ *              the right-child of the new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+
+	return (new);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This Project was done by David Chenebuah and Nina Dike Otutu comprising of Data Structures binary algorithm amd so on.
